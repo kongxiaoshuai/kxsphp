85985faab51e1e8b08ad196a42d253d74c375b4a
signK <kongxiaoshuai@hotmail.com>
2014-10-30 11:08:55 +0800

    Update hello.php
    add function _getRunTime().

--- hello.php
+++ hello.php
@@ -3,5 +3,16 @@
 	echo 'hello world,github';
 	list($msec,$sec)= microtime();
 	echo round((float)$msec+(float)$sec),4);
+	//update function start
+	/**
+	 * _getRunTime() is get current time
+	 * @access return
+	 * @return float return a float number
+	 * */
+	function _getRunTime(){
+		list ($msec,$sec)=microtime();
+		return round(((float)$msec+(float)$sec),4);
+		
+	}
 
 ?>
